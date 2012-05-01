905dc8a3f44c8b458d0758250e9bab271eb99c5b
Ahmed Kotb <kotbcorp@gmail.com>
2012-05-01 20:26:25 +0200

    added meanshift intensity+loc

--- src/meanshift.py
+++ src/meanshift.py
@@ -10,6 +10,38 @@ def meanshiftUsingIntensity(path):
     print "number of region" , numberRegions
     return segmentedImage
 
+def meanshiftUsingIntensityAndLocation(path):
+    im = cv.LoadImageM(path,cv.CV_LOAD_IMAGE_GRAYSCALE)
+    #creat a mat from the pixel intensity and its location
+    mat = cv.CreateMat(im.height,im.width,cv.CV_32FC3)
+    for i in xrange(0,im.height):
+        for j in xrange(0,im.width):
+            value = (im[i,j],i,j)
+            mat[i,j] = value
+
+    (segmentedImage, labelsImage, numberRegions) = pms.segmentMeanShift(mat)
+
+    clusters = {}
+    for i in xrange(0,labelsImage.height):
+        for j in xrange(0,labelsImage.width):
+            v = labelsImage[i,j]
+            if v in clusters:
+                clusters[v].append(im[i,j])
+            else:
+                clusters[v] =  [im[i,j]]
+
+    means = {}
+    for c in clusters:
+        means[c] = sum(clusters[c])/len(clusters[c])
+
+    for i in xrange(0,im.height):
+        for j in xrange(0,im.width):
+            lbl = labelsImage[i,j]
+            im[i,j] = means[lbl]
+
+    print "number of region" , numberRegions
+    return im
+
 def meanshiftUsingRGB(path):
     im = cv.LoadImageM(path)
     (segmentedImage, labelsImage, numberRegions) = pms.segmentMeanShift(im)
@@ -24,7 +56,10 @@ def meanshiftUsingYUV(path):
     return segmentedImage
 
 name = "../test images/single object/189080.jpg"
+#name = "../test images/single object/69015.jpg"
+
 im = meanshiftUsingIntensity(name)
+#im = meanshiftUsingIntensityAndLocation(name)
 #im = meanshiftUsingRGB(name)
 #im = meanshiftUsingYUV(name)
 
